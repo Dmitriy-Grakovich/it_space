c6262e46a43a9aae7deaa0b575d45fc6af540c13
Dmitriy-Grakovich <gudvin-gora@mail.ru>
2023-01-20 14:10:52 +0300

    ref1

--- task/src/main/java/ru/grakovich/analyzer/AnalyzerIndexImpl.java
+++ task/src/main/java/ru/grakovich/analyzer/AnalyzerIndexImpl.java
@@ -8,45 +8,45 @@ class AnalyzerIndexImpl implements AnalyzerIndex {
     @Override
     public int[][] transformer(String[] indexes) {
         int[][] result = new int[indexes.length][];
-        for (int i =0; i < indexes.length;i++) {
-         //   Arrays.stream(st.split(",")).map(x-> x.split("-"))
+        for (int i = 0; i < indexes.length; i++) {
+            //   Arrays.stream(st.split(",")).map(x-> x.split("-"))
             String[] temp = indexes[i].split(",");
             int[] arr = new int[0];
-            for (String s1:temp) {
-                arr = sumArrays(arr,stringToArraysInt(s1));
+            for (String s1 : temp) {
+                arr = sumArrays(arr, stringToArraysInt(s1));
             }
             result[i] = arr;
         }
-
-
         return result;
     }
 
-    private int[] stringToArraysInt(String string){
-        int start =Character.getNumericValue(string.charAt(0));
-        int end = Character.getNumericValue(string.charAt(string.length()-1));
+    @Override
+    public int[][] allVariants(int[][] nums) {
+
+        return new int[0][0];
+    }
+
+    private int[] stringToArraysInt(String string) {
+        int start = Character.getNumericValue(string.charAt(0));
+        int end = Character.getNumericValue(string.charAt(string.length() - 1));
         int[] result = new int[end - start + 1];
-        for (int i = 0; i< result.length;i++) {
+        for (int i = 0; i < result.length; i++) {
             result[i] = start++;
         }
         return result;
     }
-    private int[] sumArrays(int[] a, int[] b){
+
+    private int[] sumArrays(int[] a, int[] b) {
         int l = a.length;
         int r = b.length;
-        int[] result = new int[l+r];
-        for (int i = 0; i < l;i++){
+        int[] result = new int[l + r];
+        for (int i = 0; i < l; i++) {
             result[i] = a[i];
         }
-        for (int i = 0; i < r;i++){
-            result[i+l] = b[i];
+        for (int i = 0; i < r; i++) {
+            result[i + l] = b[i];
         }
         return result;
     }
 
-
-    @Override
-    public int[][] allVariants(int[][] nums) {
-        return new int[0][0];
-    }
 }
